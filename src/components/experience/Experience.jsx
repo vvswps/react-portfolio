e50d5066bc4aeb2db17eb005f8192fe0f417d7ec
vvswps <as904465@gmail.com>
2022-07-03 19:47:31 +0530

    updated with custom images and colors

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
-      <container className="container experience__container">
+      <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
@@ -89,7 +89,7 @@ const Experience = () => {
             </article>
           </div>
         </div>
-      </container>
+      </div>
     </section>
   );
 };
